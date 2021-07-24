346ca40898504b76c158bee710114ee5c4a37856
kaloyan <kaloyan.ts@abv.bg>
2021-07-24 20:59:26 +0300

    added details view

---
+++ RussianBathHouse/RussianBathHouse/Models/Accessories/AccessoryDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RussianBathHouse.Models.Accessories
+{
+    public class AccessoryDetailsViewModel
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int QuantityLeft { get; set; }
+
+    }
+}
